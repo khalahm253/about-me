a281c818acd135a600e2db47903c9f6d004ce940
‘khalahm253’ <khalahm253@gmail.com>
2018-03-06 14:30:47 -0800

    Saccfolding about-me file. First question finished

---
+++ js/app.js
@@ -0,0 +1,8 @@
+'use strict';
+var myAge = prompt('Do i look 20 years old? Please answer with "yes" or "no".').toLowerCase();
+
+if(myAge.toLowerCase() === 'yes') {
+  alert('Correct!');
+} else {
+  alert('Wrong!');
+} 
