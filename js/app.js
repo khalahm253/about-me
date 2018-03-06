ef58ae663b99e3444b184e6163495e58f3b01283
‘khalahm253’ <khalahm253@gmail.com>
2018-03-06 15:07:39 -0800

    scaffolding for about-me file. Second question and console log finshes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 'use strict';
-var myAge = prompt('Do i look 20 years old? Please answer with "yes" or "no".').toLowerCase();
+var myAge = prompt('Do you think Khalil is 20 years old? Please answer with "yes" or "no".').toLowerCase();
 
 if(myAge.toLowerCase() === 'yes') {
   alert('Correct!');
 } else {
   alert('Wrong!');
 } 
+console.log('Yes, Khalil is 20 years')
+var myCity = prompt('Do you think Khalil is from Seattle? Please answer with "yes" or "no".').toLowerCase();
+
+if(myCity.toLowerCase() === 'yes'){
+  alert('Correct!');
+}else {
+  alert('Wrong!');
+} 
+console.log('Yes, Khalil is from Seattle')
