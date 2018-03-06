9ec147d874de50a0e357aa733401dfa3f13a0721
‘khalahm253’ <khalahm253@gmail.com>
2018-03-06 15:33:25 -0800

    scaffolding file for about-me. Fourth question and fourth console log finished.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ if(myAge.toLowerCase() === 'yes') {
 } else {
   alert('Wrong!');
 } 
-console.log('Yes, Khalil is 20 years')
+console.log('Yes, Khalil is 20 years');
+
 var myCity = prompt('Do you think Khalil is from Seattle? Please answer with "yes" or "no".').toLowerCase();
 
 if(myCity.toLowerCase() === 'yes'){
@@ -14,7 +15,8 @@ if(myCity.toLowerCase() === 'yes'){
 }else {
   alert('Wrong!');
 } 
-console.log('Yes, Khalil is from Seattle')
+console.log('Yes, Khalil is from Seattle');
+
 var myHeight = prompt('Do you think Khalil is 6\'5"? Please answer with "yes" or "no".').toLowerCase();
 
 if(myHeight.toLowerCase() === 'yes'){
@@ -23,3 +25,12 @@ if(myHeight.toLowerCase() === 'yes'){
   alert('Wrong!');
 }
 console.log('Khalil is 6\'5"!')
+
+var myCar = prompt('Do you think Khalil owns a car? Please answer with "yes" or "No".').toLowerCase();
+
+if(myCar.toLowerCase() === 'yes'){
+  alert('Correct!');
+}else {
+  alert('Wrong!');
+}
+console.log('Yes! Khalil owns a Acura');
