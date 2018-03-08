173f2be2cd19d47ffb6880249fef5309747f6675
‘khalahm253’ <khalahm253@gmail.com>
2018-03-07 16:02:37 -0800

    inital scaffolding commit for day 3 about-me project.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ if(myAge.toLowerCase() === 'yes') {
   alert('Correct!');
 } else {
   alert('Wrong!');
-} 
+}
 console.log('Yes, Khalil is 20 years');
 
 var myCity = prompt('Do you think Khalil is from Seattle? Please answer with "yes" or "no".').toLowerCase();
@@ -14,7 +14,7 @@ if(myCity.toLowerCase() === 'yes'){
   alert('Correct!');
 }else {
   alert('Wrong!');
-} 
+}
 console.log('Yes, Khalil is from Seattle');
 
 var myHeight = prompt('Do you think Khalil is 6\'5"? Please answer with "yes" or "no".').toLowerCase();
@@ -44,3 +44,36 @@ if(mySport.toLowerCase() === 'no'){
 }
 console.log('Khalil plays soccer');
 
+var correctAnswer = 5;
+var guessesLeftKobe = 4;
+while(guessesLeftKobe > 0) {
+  var kobeRings = parseInt(prompt('How many rings does Kobe Bryant have?'));
+
+  if(correctAnswer === kobeRings){
+    alert('Correct!');
+    break;
+  }
+  guessesLeftKobe--;
+  alert('Wrong answer! You have ' + guessesLeftKobe + ' guesses remaining!');
+}
+
+var favoriteShoes = ['jordans', 'nike', 'adidas', 'yeezy', 'timberland'];
+
+var guessesLeft = 6;
+var flag = true;
+while(guessesLeft > 0 && flag === true) {
+  var shoeGuess = prompt('Can you guess one my favorite shoes?').toLowerCase();
+
+  for(var i = 0; i < favoriteShoes.length; i++) {
+
+    if(favoriteShoes[i] === shoeGuess){
+      alert('Correct!');
+      flag = false;
+      break;
+
+    } else {
+      alert('Wrong answer! You have ' + guessesLeft + ' guesses remaining!');
+    }
+  }
+  guessesLeft--;
+}
